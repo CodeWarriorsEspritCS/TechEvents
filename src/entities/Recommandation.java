eaf10d5b1e8bafa4860ed92656bc7a0b977bdb63
omayma <omayma@DESKTOP-OGAVQSB>
2019-06-14 13:46:30 +0200

    deleting mail

--- src/entities/Recommandation.java
+++ src/entities/Recommandation.java
@@ -14,9 +14,17 @@ public class Recommandation {
     private int idRec;
     private String description;
     private int note ;
-     private String mail;
+    
     private Evenement Intituler ;
     private Utilisateur iduserr;
+
+    public Recommandation(int idRec, String description, int note,  Evenement Intituler) {
+        this.idRec = idRec;
+        this.description = description;
+        this.note = note;
+        
+        this.Intituler = Intituler;
+    }
 
     public Recommandation() {
     }
@@ -25,30 +33,22 @@ public class Recommandation {
         this.idRec = idRec;
     }
 
-    public String getMail() {
-        return mail;
-    }
+    
 
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
 
-    public Recommandation(int idRec, String description, int note, String mail, Evenement Intituler, Utilisateur iduserr) {
+
+   
+
+    public Recommandation(int idRec, String description, int note,  Evenement Intituler, Utilisateur iduserr) {
         this.idRec = idRec;
         this.description = description;
         this.note = note;
-        this.mail = mail;
+       
         this.Intituler = Intituler;
         this.iduserr = iduserr;
     }
 
-    public Recommandation(int idRec, String description, int note, Evenement Intituler, Utilisateur iduserr) {
-        this.idRec = idRec;
-        this.description = description;
-        this.note = note;
-        this.Intituler = Intituler;
-        this.iduserr = iduserr;
-    }
+   
 
     public int getIdRec() {
         return idRec;
@@ -68,23 +68,19 @@ public class Recommandation {
 
     @Override
     public String toString() {
-        return "Recommandation{" + "idRec=" + idRec + ", description=" + description + ", note=" + note + ", mail=" + mail + ", Intituler=" + Intituler + ", iduserr=" + iduserr + '}';
+        return "Recommandation{" + "idRec=" + idRec + ", description=" + description + ", note=" + note + ",  Intituler=" + Intituler + ", iduserr=" + iduserr + '}';
     }
-
-    public Recommandation(int idRec, String description, int note, String mail) {
-        this.idRec = idRec;
-        this.description = description;
-        this.note = note;
-        this.mail = mail;
-    }
-
-   
 
     public Recommandation(int idRec, String description, int note) {
         this.idRec = idRec;
         this.description = description;
         this.note = note;
+   
     }
+
+   
+
+   
 
     public void setNote(int note) {
         this.note = note;
