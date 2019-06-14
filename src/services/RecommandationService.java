8eb7a33798a3a00618731379687e56304f7485cf
omayma <omayma@DESKTOP-OGAVQSB>
2019-06-14 13:46:55 +0200

    deleting mail

--- src/services/RecommandationService.java
+++ src/services/RecommandationService.java
@@ -18,6 +18,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import utils.BdTechEvents;
 
 /**
@@ -25,7 +27,8 @@ import utils.BdTechEvents;
  * @author Linda
  */
 public class RecommandationService implements IRecommandationService {
-Connection cnx ;
+
+    Connection cnx;
 
     public RecommandationService() {
         cnx = BdTechEvents.getInstance().getCnx();
@@ -33,31 +36,31 @@ Connection cnx ;
 
     @Override
     public void AjouterRecommandation(Recommandation e) throws SQLException {
-        PreparedStatement pst = cnx.prepareStatement("INSERT INTO `recommandation` (`idRec`, `description`, `Note`,  `mail`,`id-event`,`idUser`) VALUES (NULL, ?, ?,?,?,?)");
-       ;
- 
+        PreparedStatement pst = cnx.prepareStatement("INSERT INTO `recommandation` (`idRec`, `description`, `Note`, `id-event`,`idUser`) VALUES (NULL, ?, ?,?,?)");
+        ;
+
         pst.setString(1, e.getDescription());
         pst.setInt(2, e.getNote());
-          pst.setString(3, e.getMail());
-          pst.setInt(4, StaticVars.currentEvent.getIdEvent());
-          pst.setInt(5, StaticVars.currentUser.getIdUser());
-                 
+
+        pst.setInt(3, StaticVars.currentEvent.getIdEvent());
+        pst.setInt(4, StaticVars.currentUser.getIdUser());
+
         System.out.println(pst);
         pst.executeUpdate();
     }
 
     @Override
     public void ModifierRecommandation(Recommandation e) throws SQLException {
-          PreparedStatement pst = cnx.prepareStatement(" UPDATE `recommandation` SET `Note`=?  WHERE 'idRec' = ? ");
+        PreparedStatement pst = cnx.prepareStatement(" UPDATE `recommandation` SET `Note`=?  WHERE 'idRec' = ? ");
         pst.setInt(1, e.getIdRec());
-            pst.setInt(3, e.getNote());
+        pst.setInt(3, e.getNote());
         System.out.println(pst);
         pst.executeUpdate();
     }
 
     @Override
-    public void SupprimerRecommandation(int idRec ) throws SQLException {
-           PreparedStatement pst = cnx.prepareStatement(" DELETE FROM `recommandation` where idRec= ?");
+    public void SupprimerRecommandation(int idRec) throws SQLException {
+        PreparedStatement pst = cnx.prepareStatement(" DELETE FROM `recommandation` where idRec= ?");
         pst.setInt(1, idRec);
         System.out.println(pst);
         pst.executeUpdate();
@@ -65,36 +68,36 @@ Connection cnx ;
 
     @Override
     public ArrayList<Recommandation> ConsulterRecommandation() throws SQLException {
-        
-          PreparedStatement pst = cnx.prepareStatement("select idRec,description,Note,mail  from recommandation  " );
+
+        PreparedStatement pst = cnx.prepareStatement("select *  from recommandation  ");
         System.out.println(pst);
         ResultSet rst = pst.executeQuery();
-        ArrayList<Recommandation>listeTable=new ArrayList();
+
+        ArrayList<Recommandation> listeTable = new ArrayList();
         Recommandation u = new Recommandation();
-      
+        EvenementService i = new EvenementService();
+        UtilisateurService uss = new UtilisateurService();
         while (rst.next()) {
-        int id = rst.getInt(1);
-         String p1 = rst.getString(2);
- int not = rst.getInt(3);
-String m = rst.getString(4);
-u =new Recommandation(id,p1,not,m);
-       
-      listeTable.add(u);
-      
-         
-             
-    }    
-      return listeTable;
+            int id = rst.getInt(1);
+            String p1 = rst.getString(3);
+            int not = rst.getInt(2);
+            Evenement idEvent = i.ChercherEvenementByNom(rst.getInt(4));
+           
+            u = new Recommandation(id, p1, not,idEvent);
+
+            listeTable.add(u);
+
+        }
+        return listeTable;
     }
 
     @Override
     public ResultSet ChercherRecommandationById(int id) throws SQLException {
-Statement st = cnx.createStatement();
+        Statement st = cnx.createStatement();
         String req = "select * from recommandation where idRec=" + id;
         ResultSet rs = st.executeQuery(req);
         return rs;
-           
-        
+
     }
 
     @Override
@@ -107,5 +110,4 @@ Statement st = cnx.createStatement();
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-  
 }
